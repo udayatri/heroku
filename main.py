0782c1f39dcbbbe5a95746b2470d57abae7d0523
udayatri <udayatri6@gmail.com>
2021-11-10 18:50:57 +0530

    hnni

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("helo world")
+from flask import Flask
+
+app = Flask(__name__) #create a flask object
+
+@app.route('/')
+def index():
+  return "helo world"
+
+
+if __name__ == '__main__':
+    app.run(debug= False ) #debug enabled is creating warning and error
